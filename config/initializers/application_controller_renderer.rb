71b17ee5bdf538487eee18af78ea19707cca76b2
Hikaru1209 <h.nito2310@gmail.com>
2020-06-08 21:25:35 +0900

    [Add]基礎部分

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4ac942f9d7ec5c8a9ef1a82088abb37b86c9f7f1f57106f7f73dad135b33630cd246b9a99aa2bfdcb84a2107986a259cb782b33bb56ab4b9e1d5004ad7bc3654'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "homes#top"
+  devise_for :users
+  get "homes/about" => "homes#show", as: "homes_about"
+  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
